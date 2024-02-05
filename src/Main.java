f5f8e43cc2597ac7182eed41a0573addb4723efb
D <yuuichirou_oohashi@yahoo.co.jp>
2024-02-05 10:10:45 +0900

    配合表の読み込みの動作確認完了

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,53 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String filePath = "src/geneticPathwayList.txt";
-        String[] parentArray;
-        List<String> parentList = new ArrayList<>();
-        List<Pal> palList = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        try {
-            String line = reader.readLine();
-            parentArray = line.split(",");
-            for(String e : parentArray) {
-                palList.add(new Pal(e));
-                parentList.add(e);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for(String parent : parentList) {
+    public static void main(String[] args) {
+        final String FILEPATH = "src/geneticPathwayList.txt";
+        final char DELIMITER = ',';
+        String[][] combinationTable;
+        Scanner scanner;
+        while(true) {
             try {
-                String line = reader.readLine();
-                String[] childList = line.split(",");
-                int count = 0;
-                for(Pal pal : palList) {
-                    pal.addCombination(childList[count],parent);
-                    count++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+                scanner = new Scanner(new File(FILEPATH));
+                break;
+            } catch (FileNotFoundException e) {
+                System.out.println("配合表ファイルが見つかりませんでした\nファイルを配置したあとエンターキーを押してください");
+                scanner = new Scanner(System.in);
+                scanner.nextLine();
             }
         }
-        for(Pal pal : palList) {
-            pal.testPrint();
+        String firstLine = scanner.nextLine();
+        long delimiterCount = firstLine.chars().filter(c -> c == DELIMITER).count();
+        combinationTable = new String[(int) delimiterCount + 1][(int) delimiterCount + 1];
+        String[] firstLineParts = firstLine.split(String.valueOf(DELIMITER));
+        for(int i = 0; i <= delimiterCount; i++) {
+            combinationTable[0][i] = firstLineParts[i];
         }
+        int loopCount = 1;
+        while(scanner.hasNextLine()) {
+            String[] lineParts = scanner.nextLine().split(String.valueOf(DELIMITER));
+            combinationTable[loopCount][0] = combinationTable[0][loopCount];
+            for(int i = 1; i <= delimiterCount; i++) {
+                combinationTable[loopCount][i] = lineParts[i-1];
+            }
+            loopCount++;
+        }
+        while(true) {
+            System.out.println("どの検索方法を使用しますか" + "1.個体値遺伝検索");
+            scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    individualValueGeneticSearch(combinationTable);
+                    break;
+                default:
+                    System.out.print("だめで～す");
+                    break;
+            }
+        }
+    }
+    public static void individualValueGeneticSearch(String[][] combinationTable) {
+
     }
 }
